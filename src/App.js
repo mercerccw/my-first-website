b2b810c5bafce82217e458fb9567df49c668ec0c
eewmercer <eewmercer@gmail.com>
2021-05-16 13:51:13 -0400

    DooDoo's first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello, Treyford
+         Hello, DooDoo
         </p>
         <a
           className="App-link"
