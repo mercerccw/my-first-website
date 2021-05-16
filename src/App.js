09271853d99e0e3f973252d799b50b77cacf9267
swmercer <smercer@fdbhealth.com>
2021-05-16 14:11:37 -0400

    Stuart's first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello, DooDoo
+         Hello, Stuart
         </p>
         <a
           className="App-link"
